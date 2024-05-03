7f28bd60ff3821af32bbde60481daf4261d0cb2b
reisarthur <arthur.reis@engenharia.ufjf.br>
2024-05-03 14:49:25 -0300

    Create class Data and return boolean sucess

--- python/modules/add.py
+++ python/modules/add.py
@@ -1,2 +1,17 @@
-def process(data):
-    return data['a'] + data['b']
+class Data:
+    def __init__(self, input_dict):
+        self.a = input_dict['a']
+        self.b = input_dict['b']
+    
+    def sum(self):
+        return self.a + self.b
+
+def process(input_dict):
+    try:
+        data = Data(input_dict)
+    except KeyError:
+        return (False, "Dados de entrada devem conter 'a' e 'b'")
+    
+    result = {'soma': data.sum()}
+    
+    return (True, result)
