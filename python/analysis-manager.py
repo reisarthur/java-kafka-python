a637a923818ad46badfb04780e726d6ee854fb69
reisarthur <arthur.reis@engenharia.ufjf.br>
2024-05-03 14:48:56 -0300

    Error handling

--- python/analysis-manager.py
+++ python/analysis-manager.py
@@ -1,15 +1,12 @@
+import importlib.util
 from kafka import KafkaConsumer, KafkaProducer
 
 def import_module(module_name):
-    import importlib.util
-    try:
-        module_path = f'modules/{module_name}.py'
-        spec = importlib.util.spec_from_file_location(module_name, module_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        return module
-    except FileNotFoundError:
-        return
+    module_path = f'modules/{module_name}.py'
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
 
 
 servers = ['localhost:9092']
@@ -37,16 +34,26 @@ while True:
 
     for msg in consumer:
 
-        print(f'Mensagem recebida: key={msg.key}, value={msg.value}, headers={msg.headers}')
-        value = eval(msg.value)
+        try:
+            print(f'Mensagem recebida: key={msg.key}, headers={msg.headers}, value={msg.value}')
+            module = import_module(msg.key)
+            value = eval(msg.value)
+            value['success'], value['result'] = module.process(value['data'])
 
-        module = import_module(msg.key)
-        if module:
-            value['result'] = module.process(value['data'])
-        else:
-            value['result'] = 'Módulo não encontrado.'
+        except FileNotFoundError:
+            value['success'], value['result'] = (False, f'Módulo não encontrado: "{msg.key}"')
 
-        producer.send(output_topic, key=msg.key, value=str(value), headers=msg.headers)
-        producer.flush()
+        except SyntaxError:
+            value = {}
+            value['success'], value['result'] = (False, f'Mensagem json mal formada: "{msg.value}"')
 
-        print(f'Mensagem enviada: key={msg.key}, value={str(value)}, headers={msg.headers}')
+        except KeyError:
+            value['success'], value['result'] = (False, f'Mensagem deve conter o atributo "data": "{msg.value}"')
+
+        except:
+            value['success'], value['result'] = (False, f'Erro não reconhecido')
+
+        finally:
+            producer.send(output_topic, key=msg.key, value=str(value), headers=msg.headers)
+            producer.flush()
+            print(f'Mensagem enviada:  key={msg.key}, headers={msg.headers}, value={str(value)}')
