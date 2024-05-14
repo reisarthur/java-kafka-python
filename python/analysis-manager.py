f8af2fe5d273048dd79004e7a6726eca4348ef09
reisarthur <arthur.reis@engenharia.ufjf.br>
2024-05-14 09:31:43 -0300

    Add graceful shutdown

--- python/analysis-manager.py
+++ python/analysis-manager.py
@@ -1,5 +1,7 @@
-import importlib.util
 from kafka import KafkaConsumer, KafkaProducer
+import importlib.util
+import struct
+import signal
 
 def import_module(module_name):
     module_path = f'modules/{module_name}.py'
@@ -27,33 +29,55 @@ producer = KafkaProducer(
     value_serializer = lambda x: x.encode('utf-8')
 )
 
+class Killer:
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+        self.shutdown_signal = False
+
+    def shutdown(self, signal_no, stack_frame):
+        self.shutdown_signal = True
+        raise SystemExit
+
+killer = Killer()
+
 print('Producer and Consumer created')
 
 
-while True:
+while not killer.shutdown_signal:
 
-    for msg in consumer:
+    try:
 
-        try:
-            print(f'Mensagem recebida: key={msg.key}, headers={msg.headers}, value={msg.value}')
-            module = import_module(msg.key)
-            value = eval(msg.value)
-            value['success'], value['result'] = module.process(value['data'])
+        for msg in consumer:
 
-        except FileNotFoundError:
-            value['success'], value['result'] = (False, f'Módulo não encontrado: "{msg.key}"')
+            try:
+                print(f'Mensagem recebida: key={msg.key}, headers={msg.headers}, value={msg.value}')
+                module = import_module(msg.key)
+                value = eval(msg.value)
+                value['success'], value['result'] = module.process(value['data'])
 
-        except SyntaxError:
-            value = {}
-            value['success'], value['result'] = (False, f'Mensagem json mal formada: "{msg.value}"')
+            except FileNotFoundError:
+                value['success'], value['result'] = (False, f'Módulo não encontrado: "{msg.key}"')
 
-        except KeyError:
-            value['success'], value['result'] = (False, f'Mensagem deve conter o atributo "data": "{msg.value}"')
+            except SyntaxError:
+                value = {}
+                value['success'], value['result'] = (False, f'Mensagem json mal formada: "{msg.value}"')
 
-        except:
-            value['success'], value['result'] = (False, f'Erro não reconhecido')
+            except KeyError:
+                value['success'], value['result'] = (False, f'Mensagem deve conter o atributo "data": "{msg.value}"')
 
-        finally:
-            producer.send(output_topic, key=msg.key, value=str(value), headers=msg.headers)
-            producer.flush()
-            print(f'Mensagem enviada:  key={msg.key}, headers={msg.headers}, value={str(value)}')
+            except:
+                value['success'], value['result'] = (False, f'Erro não reconhecido')
+
+            finally:
+                producer.send(output_topic, key=msg.key, value=str(value), headers=msg.headers)
+                producer.flush()
+                print(f'Mensagem enviada:  key={msg.key}, headers={msg.headers}, value={str(value)}')
+
+    except SystemExit:
+        print('Shutting down')
+
+    finally:
+        consumer.close()
+        producer.close()
+        print('Producer and Consumer closed gracefully.')
